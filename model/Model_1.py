c6833cb6e543299077adb112f80253c24088fd86
Hawkaoaoa <78674440+Hawkaoaoa@users.noreply.github.com>
2021-08-13 11:05:38 +0800

    y

--- model/Model_1.py
+++ model/Model_1.py
@@ -49,23 +49,37 @@ class Model(torch.nn.Module):
         self.linear1 = torch.nn.Linear(3000, 500)
         self.conv1 = torch.nn.Conv1d(1, 200, kernel_size=1)
         self.pooling = torch.nn.MaxPool1d(1)
-        self.lstm = torch.nn.LSTM(input_size=500, hidden_size=100, num_layers=2)
+        self.lstm = torch.nn.LSTM(input_size=500, hidden_size=100, num_layers=1)
         self.fc1 = torch.nn.Linear(20000, 3000)
-        self.fc2 = torch.nn.Linear(3000, 100)
-        self.fc3 = torch.nn.Linear(100, 5)
+        self.fc2 = torch.nn.Linear(200, 20)
+        self.fc3 = torch.nn.Linear(20, 5)
+        self.bn1 = torch.nn.BatchNorm1d(200)
+        self.bn2 = torch.nn.BatchNorm1d(2)
+        self.dp = torch.nn.Dropout(0.3)
 
     def forward(self, x):
+
         x = F.relu(self.linear1(x))
         x = x.view(batch_size, 1, 500)
+
         x = F.relu(self.pooling(self.conv1(x)))
+        x = self.bn1(x)
+        # x = self.dp(x)
+
         x = x.view(-1, batch_size, 500)
-        hidden1 = torch.zeros(2, batch_size, 100)
-        hidden2 = torch.zeros(2, batch_size, 100)
-        x, _ = self.lstm(x, (hidden1, hidden2))
+        hidden1 = torch.zeros(1, batch_size, 100)
+        hidden2 = torch.zeros(1, batch_size, 100)
+        _, x = self.lstm(x, (hidden1, hidden2))
+
+        x = F.tanh(torch.cat([x[-1], x[-2]], dim=1))
+        x = self.bn2(x)
+        # x = self.dp(x)
+
         x = x.view(batch_size, -1)
-        x = F.relu(self.fc1(x))
+        # x = F.tanh(self.fc1(x))
         x = F.relu(self.fc2(x))
         x = self.fc3(x)
+
         return x
 
 
@@ -87,7 +101,7 @@ def train(epoch):
         optimizer.step()
 
         # running_loss += loss.item()
-        if batch_idx % 10 == 0:
+        if batch_idx % 100 == 0:
             print('[%d, %3d] loss: %.3f' % (epoch + 1, batch_idx + 1, loss.item()))
         # running_loss = 0.0
 
@@ -105,31 +119,34 @@ def test():
     print('Accuracy on test set: %d %%' % (100 * correct / total))
 
 # ========================================   Train   =======================================================
-batch_size = 16
-train_dataset = Train_Dataset('E:/data/X_1.txt', 'E:/data/y_1.txt')
-train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
-
-for i in range(5):
-    train(i)
-
+# batch_size = 1
+# train_dataset = Train_Dataset('E:/data/X_3.txt', 'E:/data/y_3.txt')
+# train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
+#
+# for i in range(20):
+#      train(i)
+#
+#
 # batch_size = 3
 # train_dataset = Train_Dataset('E:/data/X_2.txt', 'E:/data/y_2.txt')
 # train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
 #
-# for i in range(3):
-#     train(i)
-#
-# batch_size = 3
-# train_dataset = Train_Dataset('E:/data/X_3.txt', 'E:/data/y_3.txt')
-# train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
-#
-# for i in range(3):
-#     train(i)
+# for i in range(20):
+#      train(i)
+
+
+batch_size = 1
+train_dataset = Train_Dataset('E:/data/X_1.txt', 'E:/data/y_1.txt')
+train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
+
+for i in range(20):
+    train(i)
+
 
 # ==============================================  Test =========================================
 batch_size = 1
 test_dataset = Test_Dataset('E:/data/X_4.txt', 'E:/data/y_4.txt')
-test_loader = DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=True)
+test_loader = DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=False)
 
 test()
 
